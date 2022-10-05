ff537c90f6a22248b240c79dba5e3bd5f9d6ec52
thinh909thinh <51990581+thinh909thinh@users.noreply.github.com>
2022-10-05 11:04:32 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,200 @@
-import "./App.css";
-
+import { useState } from "react";
+import { Box, Container } from "@mui/material";
+import CustomButton from "./components/custom-buttom/CustomButtom";
 function App() {
-  return <div className="App"></div>;
+  const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.innerText));
+    console.log(e.target.innerText);
+  };
+  console.log(result);
+  const clear = () => {
+    setResult("");
+  };
+  const back = () => {
+    setResult(result.slice(0, -1));
+  };
+  const caculate = () => {
+    setResult(eval(result).toString());
+  };
+
+  return (
+    <Box backgroundColor="#F5F5F5">
+      <Container maxWidth="lg">
+        <Box>
+          <Box
+            margin="100px auto 0 auto"
+            minHeight="100px"
+            maxWidth={"480px"}
+            backgroundColor="gray"
+            color="white"
+            fontSize="36px"
+            display="flex"
+            justifyContent="flex-end"
+            overflow="hidden"
+          >
+            {{ result } ? result : ""}
+          </Box>
+          <Box
+            margin="0 auto 0 auto"
+            minHeight="50px"
+            maxWidth={"480px"}
+            backgroundColor="gray"
+            color="white"
+            fontSize="36px"
+            display="flex"
+            justifyContent="flex-end"
+          >
+            0
+          </Box>
+          <Box backgroundColor="#F8F8FF" maxWidth={"480px"} margin="0 auto">
+            <Box display="flex" justifyContent="space-between">
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                +
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                -
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                *
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                /
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={back}
+                sx={{
+                  backgroundColor: "#00ffce",
+                }}
+              >
+                C
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={clear}
+                sx={{
+                  backgroundColor: "red",
+                }}
+              >
+                Delete
+              </CustomButton>
+            </Box>
+            <Box
+              maxWidth={"480px"}
+              margin="0 auto"
+              display="flex"
+              flexWrap="wrap"
+              justifyContent="space-between"
+            >
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                1
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                2
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                3
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                4
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                5
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                6
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                7
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                8
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                9
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                0
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={handleClick}
+              >
+                .
+              </CustomButton>
+              <CustomButton
+                variant="contained"
+                color="secondary"
+                onClick={caculate}
+              >
+                =
+              </CustomButton>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
 
 export default App;
---
+++ src/components/custom-buttom/CustomButtom.jsx
@@ -0,0 +1,19 @@
+import MuiButton from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+
+const CustomButton = styled(MuiButton)(() => ({
+  borderRadius: "0",
+  padding: "15px 30px",
+  color: "black",
+  fontSize: "16px",
+  fontWeight: "600",
+  cursor: "pointer",
+  backgroundColor: "#F8F8FF",
+  minWidth: "80px",
+  "&:hover": {
+    backgroundColor: "#006400",
+    color: "white",
+  },
+}));
+
+export default CustomButton;
